2507cc62d01efd7a430e21d12c6dcbaf4e591e8b
Mahrukh A. Jaura <mahrukh.jaura@Mahrukhs-Air.home>
2021-11-18 17:10:46 -0500

    -.-

--- EXAM_2.R
+++ EXAM_2.R
@@ -1,1 +1,24 @@
 Mahrukh Jaura
+
+For this exam we will use a new (to you) dataset, an update of NHIS. We will look at estimates of 
+whether people worked for an employer that offered health insurance, EMPHI. The data is available on Slack.
+(Note that in data, NIU is "Not in Universe" so for instance if people are asked if their employer offers health 
+insurance then somebody not working would be NIU.)
+
+1. 
+I have run crosstabs on a subset of the data (so you cannot replicate, just use this info provided). 
+These give verbose summary of whether employer offers health insurance by educational qualification and 
+whether person is full or part time worker. Form a hypothesis test of the form, "people with *various* 
+educational qualifications who are *full or parttime* have different fraction offered health insurance." 
+I expect that you will choose different ways to operationalize educational qualification (compare above some
+level with below that level, but what level?). You can choose how to deal with NA and similar responses. I 
+expect that different people may choose different levels of significance. Please provide estimate, standard 
+error, t-stat and a p-value for the hypothesis test and a confidence interval. __Write a short paragraph__ 
+explaining the test (carefully noting what is the null hypothesis) and explaining the results of that test.
+
+Null Hypothesis: People with a bachelors degree who work fulltime have no significant difference in the fraction of people who are offered health insurance.
+Alternative Hypothesis: People with a bachelors degree who work fulltime have a different fraction of people who are offered health insurance.
+
+
+
+
